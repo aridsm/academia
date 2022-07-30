c49d7476748254eab3f68491595dcc498eaa11c7
aridsm <aniemorelanie@gmail.com>
2022-07-30 08:38:27 -0300

    responsivo

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ const hideMenu = (e) => {
 
 btnMobile.addEventListener('click', showMenuMobile)
 window.addEventListener('click', hideMenu)
+
+//modal 
+
+const modal = document.querySelector('#modal')
+const btnFecharModal = document.querySelector('#modal .btn-fechar')
+const listaGaleria = document.querySelectorAll('#galeria ul li')
+const backdropModal = document.querySelector('.backdrop')
+
+function showModal(src, alt, descricao) {
+  modal.classList.add('show');
+  const img = modal.querySelector('.content img');
+  const p = modal.querySelector('.content p');
+  img.setAttribute('src', src);
+  img.setAttribute('alt', alt);
+  p.innerText = descricao
+
+}
+
+function fecharModal() {
+  modal.classList.remove('show')
+}
+
+listaGaleria.forEach(li => {
+  const img = li.querySelector('img');
+  const src = img.getAttribute('src');
+  const alt = img.getAttribute('alt');
+  const descricao = li.dataset.descricao;
+  li.addEventListener('click', () => {
+    showModal(src, alt, descricao)
+  })
+})
+
+btnFecharModal.addEventListener('click', fecharModal)
+backdropModal.addEventListener('click', fecharModal)
